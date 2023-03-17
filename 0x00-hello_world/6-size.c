acbdf9627b4844c15040c6cc8a1fc758c1726108
Vincent Mugendi <vincentmwathi5@gmail.com>
2023-03-16 20:09:47 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,37 +1,11 @@
 #include <stdio.h>
 
-/**
- *main - Entry point
- *Return: Always 0 (Success)
- */
-
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-	return (0);
-}
-
-100-intel
-
-#!/bin/bash
-gcc -S -masm=intel $CFILE
-
-101-quote.c
-#include <stdio.h>
-#include <unistd.h>
-
-/**
- *main - Entry point
- *Return: Always 1
- *
- */
-
-int main(void)
-{
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
 }
