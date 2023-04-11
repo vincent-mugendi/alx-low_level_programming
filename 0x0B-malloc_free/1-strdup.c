f7319706f1d487dd3c5a658cface4ac3833e4602
Vincent Mugendi <vincentmwathi5@gmail.com>
2023-04-11 14:42:15 -0700

    Added task 1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *pstrdup;
+	int i, r = 0;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	aaa = malloc(sizeof(char) * (i + 1));
+
+	if (pstrdup == NULL)
+		return (NULL);
+
+	for (r = 0; str[r]; r++)
+		pstrdup[r] = str[r];
+
+	return (pstrdup);
+}
