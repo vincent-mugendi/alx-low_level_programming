4d25c2afa81da242e1201ae1252aaa49b09539a8
Vincent Mugendi <vincentmwathi5@gmail.com>
2023-04-17 16:06:04 -0700

    Added 2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - prints the name of the source code
+ * Description: The main() prints the name of the source code
+ *
+ * return: 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
