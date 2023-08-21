5a52aea34a300959b116d524de641e0237b7aabd
Vincent Mugendi <vincentmwathi5@gmail.com>
2023-08-21 07:50:41 -0700

    Added 100-operations.so file

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// C functions for arithmetic operations
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int div(int a, int b) {
+    if (b == 0) {
+        printf("Error: Division by zero!\n");
+        return 0;
+    }
+    return a / b;
+}
+
+int mod(int a, int b) {
+    if (b == 0) {
+        printf("Error: Modulo by zero!\n");
+        return 0;
+    }
+    return a % b;
+}
+
