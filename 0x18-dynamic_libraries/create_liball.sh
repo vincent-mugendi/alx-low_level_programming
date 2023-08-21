1d92a1e02d8429d6155ef610ac079a6804ca30c9
Vincent Mugendi <vincentmwathi5@gmail.com>
2023-08-21 07:31:32 -0700

    Edited liball.so

--- 0x18-dynamic_libraries/create_liball.sh
+++ 0x18-dynamic_libraries/create_liball.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-
-# Compile all .c files in the current directory into liball.so
-
-# List all .c files in the current directory and save them in a variable
-c_files=$(ls *.c)
-
-# Compile the .c files into object files
-for file in $c_files; do
-    gcc -c -fPIC $file -o ${file%.c}.o
-done
-
-# Create the dynamic library liball.so
+gcc -fPIC -c *.c
 gcc -shared -o liball.so *.o
-
-# Clean up object files
-rm *.o
-
-echo "Dynamic library liball.so created."
