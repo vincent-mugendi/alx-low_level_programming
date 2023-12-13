76250772806ed40b939e7736a67dd9151299e9f8
Vincent Mugendi <vincentmwathi5@gmail.com>
2023-12-13 02:26:16 -0800

    Debugged Task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,64 +1,61 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Performs a binary search on a sorted array of integers.
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
  *
- * @array: Pointer to the first element of the sorted integer array to search.
- * @size: Size of the array (number of elements).
- * @value: The integer value to search for.
  *
- * Returns:
- *   - The index of the value in the array (if found), or
- *   - -1 if the value is not found or the array pointer is null.
- *
- * Prints the current search range
- * (array elements being searched) at each iteration.
- *
- * Assumes:
- *   - The array is sorted in ascending order.
- *   - The target value appears only once in the array.
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
+int recursive_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
 
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	int low;
-	int high;
-	int mid;
-	int i;
+	int index;
 
-	if (!array)
-	{
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
 		return (-1);
-	}
 
-	low = 0;
-	high = (int)size - 1;
-
-	while (low <= high)
-	{
-		mid = (low + high) / 2;
-
-		printf("Searching in array: ");
-		for (i = low; i <= high; ++i)
-		{
-			printf("%d, ", array[i]);
-		}
-		printf("\n");
-
-		if (array[mid] == value)
-		{
-			return (mid);
-		}
-
-		if (array[mid] < value)
-		{
-			low = mid + 1;
-		}
-		else
-		{
-			high = mid - 1;
-		}
-	}
-
-	return (-1);
+	return (index);
 }
